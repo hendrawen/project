35e70eea1da9221e0821c059354a061f971532c2
usaid <masudsyarip808@gmail.com>
2018-05-21 09:44:10 +0800

    footer

--- application/modules/panel/views/template/footer.php
+++ application/modules/panel/views/template/footer.php
@@ -90,6 +90,10 @@
     <?php if ($this->uri->segment(1) == 'som'): ?>
       <script src="<?php echo base_url()?>assets/template/production/js/laporan.js" charset="utf-8"></script>
     <?php endif; ?>
+    
+    <?php if($this->uri->segment(1) == 'delivery'): ?>
+      <script src="<?php echo base_url()?>assets/template/production/js/delivery.js" charset="utf-8"></script>
+    <?php endif; ?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
     <script src="<?php echo base_url()?>assets/template/vendors/fullcalendar/dist/fullcalendar.min.js"></script>
     <script src="<?php echo base_url()?>assets/template/build/js/custom.min.js"></script>
