58caae71262f21337c9d28a2901361beff7cdc6e
usaid <masudsyarip808@gmail.com>
2018-07-09 12:05:46 +0800

    statusPelanggan

--- application/modules/panel/views/template/footer.php
+++ application/modules/panel/views/template/footer.php
@@ -153,6 +153,10 @@
       <script src="<?php echo base_url()?>assets/template/production/js/kas.js" charset="utf-8"></script>
     <?php endif; ?>
 
+    <?php if ($this->uri->segment(1) == 'status_pelanggan'): ?>
+      <script src="<?php echo base_url()?>assets/template/production/js/status_pelanggan.js" charset="utf-8"></script>
+    <?php endif; ?>
+
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
     <script src="<?php echo base_url()?>assets/template/vendors/fullcalendar/dist/fullcalendar.min.js"></script>
---
+++ assets/template/production/js/status_pelanggan.js
@@ -0,0 +1,68 @@
+var table;
+var btn_excel = $("#excel-tracking");
+ 
+$(document).ready(function() {
+    $('#filter-kota').select2();
+    $('#filter-kecamatan').select2();
+    $('#tahun-tracking').select2();
+    
+    //datatables
+    table = $('#table-tracking').DataTable({ 
+
+        "processing": true, //Feature control the processing indicator.
+        "serverSide": true, //Feature control DataTables' server-side processing mode.
+        "order": [], //Initial no order.
+ 
+        // Load data for the table's content from an Ajax source
+        "ajax": {
+            "url": base_url+"status_pelanggan/ajax_list",
+            "type": "POST",
+            "data": function ( data ) {
+                data.tahun = $('#tahun-tracking').val();
+                data.kota = $('#filter-kota').val();
+                data.kecamatan = $('#filter-kecamatan').val();
+            }
+        },
+
+        //Set column definition initialisation properties.
+        "columnDefs": [
+        { 
+            "targets": [0], //first column / numbering column
+            "orderable": false, //set not orderable
+        },
+        ],
+ 
+    });
+ 
+    $('#btn-filter-tracking').click(function(){
+        reload_table_tracking();
+        
+    });
+
+    $('#btn-reset-tracking').click(function(){ 
+        $('#form-tracking')[0].reset();
+        reload_table_tracking();
+    });
+});
+
+function reload_table_tracking()
+{
+    $('#table-tracking').DataTable().ajax.reload();//reload datatable ajax
+}
+
+$(btn_excel).click(function (e) {
+    tahun = $('#tahun-tracking').val();
+    kota = $('#filter-kota').val();
+    kecamatan = $('#filter-kecamatan').val();
+    if (tahun == '') {
+        tahun = 'all';
+    }
+    if (kota == '') {
+        kota = 'all';
+    }
+    if (kecamatan == '') {
+        kecamatan = 'all';
+    }
+    
+    window.location = base_url + 'tracking/download_excel/'+tahun+'/'+kota+'/'+kecamatan;
+});
