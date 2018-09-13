36537001ff8f612adc60dfadc3a12274fa546cfb
usaidmasud <masudsyarip808@gmail.com>
2018-09-13 13:37:23 +0700

    edit penarikan cip

--- application/modules/penarikan/models/Penarikan_model.php
+++ application/modules/penarikan/models/Penarikan_model.php
@@ -11,137 +11,187 @@ class Penarikan_model extends CI_Model{
   'wp_pelanggan.id_pelanggan', 'wp_pelanggan.nama_pelanggan', 'wp_barang.nama_barang', 'SUM(wp_asis_debt.turun_krat) as qty', 'wp_barang.satuan', 'wp_pelanggan.kelurahan', 'wp_asis_debt.tanggal as tgl_penarikan', 'wp_pelanggan.kecamatan', 'wp_pelanggan.no_telp', 'wp_karyawan.nama', 'wp_transaksi.username', 'SUM(wp_asis_debt.turun_krat) AS total', 'SUM(wp_asis_debt.bayar_krat) as bayar_krat', 'b.nama as nama_debt', 'wp_asis_debt.bayar_uang'); //set column field database for datatable searchable
   var $order = array('wp_transaksi.id_transaksi' => 'DESC'); // default order
 
-  public function __construct()
-  {
-      parent::__construct();
-  }
-
-  private function _get_datatables_query()
-  {
-
-    //add custom filter here
-    $this->db->select("wp_asis_debt.id,wp_transaksi.id_transaksi, wp_transaksi.tgl_transaksi, DATE_ADD(wp_transaksi.tgl_transaksi, INTERVAL 14 DAY) as jatuh_tempo, wp_pelanggan.id_pelanggan, wp_pelanggan.nama_pelanggan, wp_barang.nama_barang, SUM(wp_asis_debt.turun_krat) as qty, wp_barang.satuan, wp_pelanggan.kelurahan, wp_asis_debt.tanggal as tgl_penarikan, wp_pelanggan.kecamatan,wp_pelanggan.no_telp, wp_karyawan.nama, wp_transaksi.username, SUM(wp_asis_debt.turun_krat) AS total, SUM(wp_asis_debt.bayar_krat) as bayar_krat, b.nama as nama_debt, wp_asis_debt.bayar_uang, sum(wp_asis_debt.bayar_krat+(wp_asis_debt.bayar_uang/wp_krat_kosong.harga)) as jumlah, (sum(wp_asis_debt.turun_krat) - sum(wp_asis_debt.bayar_krat+(wp_asis_debt.bayar_uang/wp_krat_kosong.harga))) as sisa, IF (sum(wp_asis_debt.turun_krat) > (sum(wp_asis_debt.bayar_krat + (wp_asis_debt.bayar_uang/wp_krat_kosong.harga))), 'Masih ada ASET', 'Tidak ada ASET') as `status`");
-    $this->db->from('wp_asis_debt,wp_krat_kosong');
-    $this->db->join('wp_transaksi', 'wp_asis_debt.id_transaksi = wp_transaksi.id', 'left');
-    $this->db->join('wp_pelanggan', 'wp_asis_debt.wp_pelanggan_id = wp_pelanggan.id', 'left');
-    $this->db->join('wp_barang', 'wp_asis_debt.wp_barang_id = wp_barang.id', 'left');
-    $this->db->join('wp_karyawan', 'wp_pelanggan.wp_karyawan_id_karyawan = wp_karyawan.id_karyawan');
-    $this->db->group_by('wp_transaksi.id_transaksi');
-    
-    $this->db->join('wp_karyawan as b', 'b.id_karyawan = wp_transaksi.username');
-
-    // $this->db->from($this->table);
-    $i = 0;
-
-    foreach ($this->column_search as $item) // loop column
+    public function __construct()
     {
-        if($_POST['search']['value']) // if datatable send POST for search
+        parent::__construct();
+    }
+
+    private function _get_datatables_query()
+    {
+
+        //add custom filter here
+        $this->db->select("wp_asis_debt.id,wp_transaksi.id_transaksi, wp_transaksi.tgl_transaksi, DATE_ADD(wp_transaksi.tgl_transaksi, INTERVAL 14 DAY) as jatuh_tempo, wp_pelanggan.id_pelanggan, wp_pelanggan.nama_pelanggan, wp_barang.nama_barang, SUM(wp_asis_debt.turun_krat) as qty, wp_barang.satuan, wp_pelanggan.kelurahan, wp_asis_debt.tanggal as tgl_penarikan, wp_pelanggan.kecamatan,wp_pelanggan.no_telp, wp_karyawan.nama, wp_transaksi.username, SUM(wp_asis_debt.turun_krat) AS total, SUM(wp_asis_debt.bayar_krat) as bayar_krat, b.nama as nama_debt, wp_asis_debt.bayar_uang, sum(wp_asis_debt.bayar_krat+(wp_asis_debt.bayar_uang/wp_krat_kosong.harga)) as jumlah, (sum(wp_asis_debt.turun_krat) - sum(wp_asis_debt.bayar_krat+(wp_asis_debt.bayar_uang/wp_krat_kosong.harga))) as sisa, IF (sum(wp_asis_debt.turun_krat) > (sum(wp_asis_debt.bayar_krat + (wp_asis_debt.bayar_uang/wp_krat_kosong.harga))), 'Masih ada ASET', 'Tidak ada ASET') as `status`");
+        $this->db->from('wp_asis_debt,wp_krat_kosong');
+        $this->db->join('wp_transaksi', 'wp_asis_debt.id_transaksi = wp_transaksi.id', 'left');
+        $this->db->join('wp_pelanggan', 'wp_asis_debt.wp_pelanggan_id = wp_pelanggan.id', 'left');
+        $this->db->join('wp_barang', 'wp_asis_debt.wp_barang_id = wp_barang.id', 'left');
+        $this->db->join('wp_karyawan', 'wp_pelanggan.wp_karyawan_id_karyawan = wp_karyawan.id_karyawan');
+        $this->db->group_by('wp_transaksi.id_transaksi');
+        
+        $this->db->join('wp_karyawan as b', 'b.id_karyawan = wp_transaksi.username');
+
+        // $this->db->from($this->table);
+        $i = 0;
+
+        foreach ($this->column_search as $item) // loop column
         {
-
-            if($i===0) // first loop
+            if($_POST['search']['value']) // if datatable send POST for search
             {
-                $this->db->group_start(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
-                $this->db->like($item, $_POST['search']['value']);
-            }
-            else
-            {
-                $this->db->or_like($item, $_POST['search']['value']);
-            }
 
-            if(count($this->column_search) - 1 == $i) //last loop
-                $this->db->group_end(); //close bracket
+                if($i===0) // first loop
+                {
+                    $this->db->group_start(); // open bracket. query Where with OR clause better with bracket. because maybe can combine with other WHERE with AND.
+                    $this->db->like($item, $_POST['search']['value']);
+                }
+                else
+                {
+                    $this->db->or_like($item, $_POST['search']['value']);
+                }
+
+                if(count($this->column_search) - 1 == $i) //last loop
+                    $this->db->group_end(); //close bracket
+            }
+            $i++;
         }
-        $i++;
+
+        if(isset($_POST['order'])) // here order processing
+        {
+            $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+        }
+        else if(isset($this->order))
+        {
+            $order = $this->order;
+            $this->db->order_by(key($order), $order[key($order)]);
+        }
     }
 
-    if(isset($_POST['order'])) // here order processing
+    public function get_by_id($no_faktur)
     {
-        $this->db->order_by($this->column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+        // $this->db->select('wp_asis_debt.id, wp_asis_debt.turun_krat, wp_asis_debt.piutang, 
+        // wp_pelanggan.id_pelanggan, wp_pelanggan.nama_pelanggan, wp_asis_debt.tanggal as tgl_penarikan, wp_asis_debt.bayar_krat, wp_asis_debt.bayar_uang, wp_penarikan.id_penarikan');
+        
+        // $this->db->where("wp_asis_debt.piutang <> wp_asis_debt.turun_krat");
+
+        // $this->db->join('wp_pelanggan', 'wp_pelanggan.id = wp_asis_debt.wp_pelanggan_id', 'inner');
+        // $this->db->join('wp_penarikan', 'wp_penarikan.wp_asis_debt_id = wp_asis_debt.id', 'left');
+        // $this->db->join('wp_transaksi', 'wp_transaksi.id = wp_asis_debt.id_transaksi', 'inner');
+        
+        // $this->db->where('wp_transaksi.id_transaksi', $no_faktur);
+        
+        // $this->db->group_by('wp_asis_debt.id');
+
+        // $this->db->order_by('wp_asis_debt.id', 'asc');
+
+        // $record = $this->db->get('wp_asis_debt');
+
+        // return $record->result();
+
+        /**
+         * Baru
+         */
+        $this->db->select('wp_penarikan.*, wp_pelanggan.id_pelanggan, wp_pelanggan.nama_pelanggan, wp_asis_debt_id as id_asis_debt');
+        $this->db->join('wp_asis_debt', 'wp_asis_debt.id = wp_penarikan.wp_asis_debt_id', 'inner');
+        $this->db->join('wp_transaksi', 'wp_transaksi.id = wp_asis_debt.id_transaksi', 'inner');
+        $this->db->join('wp_pelanggan', 'wp_pelanggan.id = wp_penarikan.wp_pelanggan_id', 'inner');
+        $this->db->where('wp_transaksi.id_transaksi', $no_faktur);
+        return $this->db->get('wp_penarikan')->result();
     }
-    else if(isset($this->order))
+
+    public function get_datatables()
     {
-        $order = $this->order;
-        $this->db->order_by(key($order), $order[key($order)]);
+        $this->_get_datatables_query();
+        if($_POST['length'] != -1)
+        $this->db->limit($_POST['length'], $_POST['start']);
+        $query = $this->db->get();
+        return $query->result();
     }
- }
 
- public function get_by_id($no_faktur)
- {
-    $this->db->select('wp_asis_debt.id, wp_asis_debt.turun_krat, wp_asis_debt.piutang, 
-    wp_pelanggan.id_pelanggan, wp_pelanggan.nama_pelanggan, wp_asis_debt.tanggal as tgl_penarikan, wp_asis_debt.bayar_krat, wp_asis_debt.bayar_uang');
-    $this->db->where("wp_asis_debt.piutang <> wp_asis_debt.turun_krat");
+    public function count_filtered()
+    {
+        $this->_get_datatables_query();
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
 
-    $this->db->join('wp_pelanggan', 'wp_pelanggan.id = wp_asis_debt.wp_pelanggan_id', 'inner');
-    $this->db->join('wp_penarikan', 'wp_penarikan.wp_asis_debt_id = wp_asis_debt.id', 'left');
-    $this->db->join('wp_transaksi', 'wp_transaksi.id = wp_asis_debt.id_transaksi', 'inner');
-    
-    $this->db->where('wp_transaksi.id_transaksi', $no_faktur);
-    
-    $this->db->group_by('wp_asis_debt.id');
+    public function count_all()
+    {
+        $this->db->from($this->table);
+        return $this->db->count_all_results();
+    }
 
-    $this->db->order_by('wp_asis_debt.id', 'asc');
+    function cari_pelanggan($idpelanggan){
+            $this->db->like('id_pelanggan', $idpelanggan , 'both');
+            $this->db->order_by('id_pelanggan', 'ASC');
+            $this->db->where('utang <=', 'bayar');
+            $this->db->limit(10);
+            return $this->db->get('v_detail_utang')->result();
+        }
 
-    $record = $this->db->get('wp_asis_debt');
+    function update($id, $data)
+    {
+        $this->db->where($this->id2, $id);
+        return $this->db->update($this->table2, $data);
+    }
 
-    return $record->result();
- }
+    function get_faktur($faktur)
+    {
+        // wp_asis_debt, wp_penarikan
+        $this->db->select('wp_asis_debt.id, wp_transaksi.id_transaksi as faktur, nama_pelanggan');
+        $this->db->join('wp_pelanggan', 'wp_pelanggan.id = wp_asis_debt.wp_pelanggan_id', 'inner');
+        $this->db->join('wp_transaksi', 'wp_transaksi.id = wp_asis_debt.id_transaksi', 'inner');
+        
+        
+        $this->db->where('wp_asis_debt.id', $faktur);
+        return $this->db->get('wp_asis_debt')->row();
+    }
 
- public function get_datatables()
- {
-    $this->_get_datatables_query();
-    if($_POST['length'] != -1)
-    $this->db->limit($_POST['length'], $_POST['start']);
-    $query = $this->db->get();
-    return $query->result();
- }
+    function hapus_pembayaran($faktur)
+    {
+        
+        $this->db->where('wp_penarikan.wp_asis_debt_id', $faktur);
+        $this->db->delete('wp_penarikan');
+        
+        $this->db->where('wp_asis_debt.id', $faktur);
+        $this->db->delete('wp_asis_debt');
+    }
 
- public function count_filtered()
- {
-     $this->_get_datatables_query();
-     $query = $this->db->get();
-     return $query->num_rows();
- }
+    function get_penarikan($id)
+    {
+        $this->db->select('wp_penarikan.*, wp_asis_debt.id as id_asis_debt');
+        
+        $this->db->where('id_penarikan', $id);
+        $this->db->join('wp_asis_debt', 'wp_asis_debt.id = wp_penarikan.wp_asis_debt_id', 'inner');
+        return $this->db->get('wp_penarikan')->row();
+    }
 
- public function count_all()
- {
-     $this->db->from($this->table);
-     return $this->db->count_all_results();
- }
+    public function update_penarikan($penarikan, $asis_debt)
+    {
+        //   updating wp_penarikan
+        $id_penarikan = $penarikan['id_penarikan'];
+        unset($penarikan['id_penarikan']);
+        $this->db->where('id_penarikan', $id_penarikan);
+        $this->db->update('wp_penarikan', $penarikan);
+        // updating wp_asis_debt
+        $id = $asis_debt['id'];
+        unset($asis_debt['id']);
+        $this->db->where('id', $id);
+        $this->db->update('wp_asis_debt', $asis_debt);
 
-  function cari_pelanggan($idpelanggan){
-		$this->db->like('id_pelanggan', $idpelanggan , 'both');
-		$this->db->order_by('id_pelanggan', 'ASC');
-        $this->db->where('utang <=', 'bayar');
-		$this->db->limit(10);
-		return $this->db->get('v_detail_utang')->result();
-	}
+    }
 
-  function update($id, $data)
-  {
-      $this->db->where($this->id2, $id);
-      return $this->db->update($this->table2, $data);
-  }
+    public function get_asis_debt($id)
+    {
+        $this->db->select('id, bayar_krat, bayar_uang, piutang');
+        $this->db->where('id', $id);
+        return $this->db->get('wp_asis_debt')->row();
+    }
 
-  function get_faktur($faktur)
-  {
-      // wp_asis_debt, wp_penarikan
-      $this->db->select('wp_asis_debt.id, wp_transaksi.id_transaksi as faktur, nama_pelanggan');
-      $this->db->join('wp_pelanggan', 'wp_pelanggan.id = wp_asis_debt.wp_pelanggan_id', 'inner');
-      $this->db->join('wp_transaksi', 'wp_transaksi.id = wp_asis_debt.id_transaksi', 'inner');
-      
-      
-      $this->db->where('wp_asis_debt.id', $faktur);
-      return $this->db->get('wp_asis_debt')->row();
-  }
-
-  function hapus_pembayaran($faktur)
-  {
-      
-      $this->db->where('wp_penarikan.wp_asis_debt_id', $faktur);
-      $this->db->delete('wp_penarikan');
-      
-      $this->db->where('wp_asis_debt.id', $faktur);
-      $this->db->delete('wp_asis_debt');
-  }
-
+    public function harga_krat_kosong()
+    {
+        $this->db->select('harga');
+        $this->db->where('id', 1);
+        $hasil = $this->db->get('wp_krat_kosong', 0, 1)->row();
+        return $hasil->harga;
+    }
 }
+
--- application/modules/penarikan/views/ubah.php
+++ application/modules/penarikan/views/ubah.php
@@ -1,13 +1,4 @@
 
-
-
-<?php
-
-echo "<pre>";
-print_r ($detail);
-echo "</pre>";
-
-?>
 <?php if ($this->session->flashdata('message')): ?>
 <div class="row">
     <div class="col-md-12 col-sm-12 col-xs-12">
@@ -26,7 +17,7 @@ echo "</pre>";
 
 <div class="x_panel">
     <div class="x_title">
-        <h2>Cek Aset</h2>
+        <h2><?php echo $detail[0]->nama_pelanggan .' : ['.$detail[0]->id_pelanggan.']' ?></h2>
         <ul class="nav navbar-right panel_toolbox">
             <li>
                 <a class="collapse-link">
@@ -60,41 +51,16 @@ echo "</pre>";
         <div class="clearfix"></div>
     </div>
     <div class="x_content">
-
-        <!-- start form for validation -->
-        <!-- <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12 form-group">
-                <label for="fullname">ID Pelanggan * :</label>
-                <input
-                    type="text"
-                    id="id_track_aset"
-                    value="<?=$detail[0]->id_pelanggan?>"
-                    class="form-control"
-                    placeholder="Masukkan ID Pelanggan"
-                    name="id_track_aset"
-                    readonly="true"
-                    required="">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12 form-group text-right">
-                <button id="button_aset" class="btn btn-success">
-                    <i class="fa fa-search"></i>
-                    Cek</button>
-            </div>
-        </div> -->
         <div class="row">
             <div class="table-responsive">
                 <table
                     class="table jambo_table table-bordered dt-responsive nowrap"
                     id="tabel_cari_aset">
                     <thead>
-                        <th>ID Pelanggan</th>
-                        <th>Nama Pelanggan</th>
-                        <th>Piutang</th>
                         <th>Tanggal Penarikan</th>
                         <th>Bayar Krat</th>
                         <th>Bayar Uang (Rp.)</th>
+                        <th>Action</th>
                     </thead>
                     <tbody>
 
@@ -103,12 +69,12 @@ echo "</pre>";
                         if ($key->bayar_krat > 0 || $key->bayar_uang > 0 ):
                     ?>
                     <tr>
-                    <td><?=$key->id_pelanggan?></td>
-                    <td><?=$key->nama_pelanggan?></td>
-                    <td><?=$key->nama_pelanggan?></td>
                     <td><?=tgl_indo($key->tgl_penarikan)?></td>
                     <td><?=$key->bayar_krat?></td>
                     <td><?=$key->bayar_uang?></td>
+                    <td>
+                    <button type="button" onClick="<?php echo 'ubah_penarikan('.$key->id_penarikan.')' ?>" class="btn btn-success btn-xs">Edit</button>
+                    </td>
                     </tr>
                     <?php
                     endif;
@@ -116,6 +82,9 @@ echo "</pre>";
                     ?>
                     </tbody>
                 </table>
+                
+                <a href="<?=site_url('penarikan')?>" type="button" class="btn btn-danger">Kembali</a>
+                
             </div>
         </div>
         <!-- end form for validations -->
@@ -123,124 +92,82 @@ echo "</pre>";
     </div>
 </div>
 
-<div class="x_panel">
-    <div class="x_title">
-        <h2>Form Pembayaran/Penarikan Aset</h2>
-        <ul class="nav navbar-right panel_toolbox">
-            <li>
-                <a class="collapse-link">
-                    <i class="fa fa-chevron-up"></i>
-                </a>
-            </li>
-            <li class="dropdown">
-                <a
-                    href="#"
-                    class="dropdown-toggle"
-                    data-toggle="dropdown"
-                    role="button"
-                    aria-expanded="false">
-                    <i class="fa fa-wrench"></i>
-                </a>
-                <ul class="dropdown-menu" role="menu">
-                    <li>
-                        <a href="#">Settings 1</a>
-                    </li>
-                    <li>
-                        <a href="#">Settings 2</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a class="close-link">
-                    <i class="fa fa-close"></i>
-                </a>
-            </li>
-        </ul>
-        <div class="clearfix"></div>
-    </div>
-    <div class="x_content">
 
-        <form action="#" id="form-tarik-aset" method="post">
-            <div id="pesan-post"></div>
-            <div class="col-md-3 col-sm-3 col-lg-3 col-xs-12 form-group">
-                <label for="fullname">Tanggal Bayar/Tarik * :</label>
-                <div class="controls">
-                    <div class="col-md-12 xdisplay_inputx form-group has-feedback">
+<div class="modal fade" id="modal_edit_penarikan" data-backdrop="static">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Ubah Data Penarikan</h4>
+            </div>
+            <div class="modal-body">
+                <form action="#" id="form-tarik-aset-update" method="post">
+                <div id="pesan-post"></div>
+                    <div class="form-group">
+                        <label for="fullname">Tanggal Bayar/Tarik * :</label>
+                        <input
+                            type="date"
+                            class="form-control"
+                            id="tgl_bayar"
+                            name="tgl_bayar"
+                            placeholder="Tanggal Transaksi">
+                    </div>
+                    <div id="form_bayar_uang"
+                        class="form-group">
+                        <label for="bayar_uang">Bayar Uang (Rp.) *</label>
                         <input
                             type="text"
-                            class="form-control has-feedback-left"
-                            id="single_cal3"
-                            name="tgl_bayar"
-                            placeholder="Tanggal Transaksi"
-                            aria-describedby="inputSuccess2Status3">
-                        <span class="fa fa-calendar-o form-control-feedback left" aria-hidden="true"></span>
-                        <span id="inputSuccess2Status3" class="sr-only">(success)</span>
+                            id="bayar_uang"
+                            name="bayar_uang"
+                            class="form-control"
+                            onkeyup="FormatCurrency(this)"
+                            autocomplete="off"
+                            placeholder="Masukkan jumlah bayar">
                     </div>
-                </div>
+                    <div id="form_bayar_krat"
+                        class="form-group">
+                        <label for="bayar_krat">Bayar Krat *</label>
+                        <input
+                            type="text"
+                            id="bayar_krat"
+                            name="bayar_krat"
+                            class="form-control"
+                            autocomplete="off"
+                            placeholder="Masukkan jumlah bayar">
+                    </div>
+                    <div
+                        class="form-group">
+                        <label for="bayar_krat">Gudang</label>
+                        <select name="gudang" class="form-control" id="gudang">
+                            <option value="">Pilih Gudang</option>
+                            <?php 
+                        foreach ($gudang->result() as $key) {?>
+                            <option value="<?php echo $key->id ?>"><?php echo $key->nama_gudang ?></option>
+                            <?php }
+                        ?>
+                        </select>
+                    </div>
+                    <!-- <input type="hidden" name="sudah" id="sudah"> -->
+                    <input
+                        type="hidden"
+                        name="<?=$this->security->get_csrf_token_name();?>"
+                        value="<?=$this->security->get_csrf_hash();?>">
+                    <input type="hidden" id="id_penarikan" name="id_penarikan"/>
+                    <input type="hidden" id="id_asis_debt" name="id_asis_debt"/>
+                </form>
             </div>
-            <div class="col-md-3 col-sm-3 col-lg-3 col-xs-12 form-group">
-                <label for="jenis">Jenis Pembayaran * :</label>
-                <select name="jenis" id="jenis" class="form-control">
-                    <option value="krat" selected="selected">Krat</option>
-                    <option value="uang">Uang</option>
-                </select>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" onClick="update_data_penarikan()" class="btn btn-primary">Save changes</button>
             </div>
-            <div
-                id="form-input-uang"
-                class="col-md-3 col-sm-3 col-lg-3 col-xs-12 form-group">
-                <label for="bayar_uang">Bayar Uang (Rp.) *</label>
-                <input
-                    type="text"
-                    id="bayar_uang"
-                    class="form-control"
-                    onkeyup="FormatCurrency(this)"
-                    autocomplete="off"
-                    placeholder="Masukkan jumlah bayar">
-            </div>
-            <div
-                id="form-input-krat"
-                class="col-md-3 col-sm-3 col-lg-3 col-xs-12 form-group">
-                <label for="bayar_krat">Bayar Krat *</label>
-                <input
-                    type="text"
-                    id="bayar_krat"
-                    class="form-control"
-                    autocomplete="off"
-                    placeholder="Masukkan jumlah bayar">
-            </div>
-            <div
-                id="form-input-krat"
-                class="col-md-3 col-sm-3 col-lg-3 col-xs-12 form-group">
-                <label for="bayar_krat">Gudang</label>
-                <select name="gudang" class="form-control" id="gudang">
-                    <option value="">Pilih Gudang</option>
-                    <?php 
-                  foreach ($gudang->result() as $key) {?>
-                    <option value="<?php echo $key->id ?>"><?php echo $key->nama_gudang ?></option>
-                    <?php }
-                ?>
-                </select>
-            </div>
-            <!-- <input type="hidden" name="sudah" id="sudah"> -->
-            <input
-                type="hidden"
-                name="<?=$this->security->get_csrf_token_name();?>"
-                value="<?=$this->security->get_csrf_hash();?>">
-            <input type="hidden" id="id" name="id"/>
-
-            <div class="form-group text-right">
-                <a
-                    href="<?php echo base_url('pembayaran'); ?>"
-                    type="button"
-                    class="btn btn-default">
-                    <i class="fa fa-arrow-left"></i>
-                    Kembali</a>
-                <button type="button" id="btn-bayar-aset" class="btn btn-success">
-                    <i class="fa fa-credit-card"></i>
-                    Bayar</button>
-            </div>
-
-        </form>
-        <!-- end form for validations -->
+        </div>
     </div>
 </div>
+
+<?php
+
+// echo "<pre>";
+// print_r ($detail);
+// echo "</pre>";
+
+?>
